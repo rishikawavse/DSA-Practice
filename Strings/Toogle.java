7ee9d74227c2317af1fd16e552157de7b38cac2d
Rishika Wavse <rishikawavse@gmail.com>
2025-08-15 09:16:38 +0530

    toogle strings

--- Strings/Toogle.java
+++ Strings/Toogle.java
@@ -1,5 +1,26 @@
 package Strings;
 
-public class Toogle {
+import java.util.Scanner;
 
+public class Toogle {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        StringBuilder sb=new StringBuilder(sc.nextLine());
+        for(int i=0;i<sb.length();i++){
+            boolean flag=true;
+            char ch= sb.charAt(i);
+            if(ch==' ') continue;;
+            int ascii=(int)ch;
+            if(ascii>=97) flag=false;
+            if(flag==true){
+                ascii+=32;
+                char dh=(char)ascii;
+                sb.setCharAt(i,dh);
+            }else{
+                ascii-=32;
+                char dh=(char)ascii;
+                sb.setCharAt(i,dh);
+            }
+        }
+    }
 }
