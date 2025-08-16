45830c385fbd131c477cf08ebdd84ca617927bd7
Rishika Wavse <rishikawavse@gmail.com>
2025-08-16 18:29:29 +0530

    compressing string such that duplicate characters are replaced with character and followed by number of consecutive duplicates

---
+++ Strings/StringCompress.java
@@ -0,0 +1,22 @@
+package Strings;
+
+public class StringCompress {
+    public static void main(String[] args) {
+        String str="aaabbbbbcccdde";
+        String ans=""+str.charAt(0);
+        int cnt=1;
+        for(int i=1;i<str.length();i++){
+            char curr=str.charAt(i);
+            char prev=str.charAt(i-1);
+            if(curr==prev){
+                cnt++;
+            }else{
+                ans+=cnt;
+                cnt=1;
+                ans+=curr;
+            }
+        }
+        ans+=cnt;
+        System.out.println(ans);
+    }
+}
